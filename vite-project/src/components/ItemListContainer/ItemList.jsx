aef84af8a2a93674dd36fbc49711eccf45d9a266
Juan Aliaga <juanialiaga4@gmail.com>
2023-08-25 14:36:58 -0300

    agregado de estilos a cards

--- vite-project/src/components/ItemListContainer/ItemList.jsx
+++ vite-project/src/components/ItemListContainer/ItemList.jsx
@@ -6,11 +6,12 @@ const ItemList = ({item,loading}) => {
     return (
         <div>
             <h2>Productos</h2>
-            <div className="contenedor">
+            <div className="card-container" style={{display: "flex", flexWrap: "wrap", gap: "10px"}}>
                 {item.map((p) => (
-                    <div key={p.id} className="card">
-                        <h5 className="card-tittle">{p.nombre}</h5>
-                        <p className="card-text">${p.precio}</p>
+                    <div key={p.id} className="card" style={{flex: "0", minWidth: "360px"}}>
+                        <h5 className="card-tittle text-center">{p.nombre}</h5>
+                        <p className="card-text ms-1 mb-2 fw-bold text-start">${p.precio}</p>
+                        <a href="#" class="btn btn-primary w-50 m-auto mb-1">Comprar</a>
                     </div>
                 ))}
             </div>
