091a7e701a9c2ab29c872ed5ad34b75298e60e3b
Juan Aliaga <juanialiaga4@gmail.com>
2023-09-14 11:36:13 -0300

    se agrego fireStore y se elimino los productos mockeados

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -3,9 +3,20 @@ import ReactDOM from 'react-dom/client'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap-icons/font/bootstrap-icons.css"
 import {BrowserRouter} from "react-router-dom"
+import { initializeApp } from "firebase/app";
 import App from './App.jsx'
 import './index.css'
 
+const firebaseConfig = {
+  apiKey: "AIzaSyD3FW3Mi_VNPhSNxDQUCC93N-CTtn4KJSc",
+  authDomain: "proyecto-react-88105.firebaseapp.com",
+  projectId: "proyecto-react-88105",
+  storageBucket: "proyecto-react-88105.appspot.com",
+  messagingSenderId: "211589215162",
+  appId: "1:211589215162:web:3974c14361c3f2daa02383"
+};
+
+initializeApp(firebaseConfig);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- vite-project/src/productos.js
+++ vite-project/src/productos.js
@@ -1,34 +1,56 @@
-const productos = [
-    {id: '1', nombre: "Teclado Hyperx Alloy Fps",precio: 500, categoria: "Perifericos"},
-    {id: '2', nombre: "Teclado Hyperx Alloy Origin",precio: 600, categoria: "Perifericos"},
-    {id: '3', nombre: "Monitor ASUS VA27EHE ",precio: 1000, categoria: "Monitores"},
-    {id: '4', nombre: "Monitor AOC G2490VX",precio: 900, categoria: "Monitores"},
-    {id: '5', nombre: "Notebook Banghó L4", precio: 1500, categoria: "Notebooks"},
-    {id: '6', nombre: "Notebook Tuf F15", precio: 1400, categoria: "Notebooks"},
-    {id: '7', nombre: "RTX 3080 Ti ", precio: 2000, categoria: "GPU"},
-    {id: '8', nombre: "RTX 3070 GAMING", precio: 1700, categoria: "GPU"},
-    {id: '9', nombre: "AMD Ryzen 9 5900X", precio: 1550, categoria: "CPU"},
-    {id: '10', nombre: "AMD Ryzen 7 5800X", precio: 1450, categoria: "CPU"}
-]
+import {doc, getDoc, collection, getDocs, getFirestore, query, where} from "firebase/firestore"
+
+// const productos = [
+//     {id: '1', nombre: "Teclado Hyperx Alloy Fps",precio: 500, categoria: "Perifericos"},
+//     {id: '2', nombre: "Teclado Hyperx Alloy Origin",precio: 600, categoria: "Perifericos"},
+//     {id: '3', nombre: "Monitor ASUS VA27EHE ",precio: 1000, categoria: "Monitores"},
+//     {id: '4', nombre: "Monitor AOC G2490VX",precio: 900, categoria: "Monitores"},
+//     {id: '5', nombre: "Notebook Banghó L4", precio: 1500, categoria: "Notebooks"},
+//     {id: '6', nombre: "Notebook Tuf F15", precio: 1400, categoria: "Notebooks"},
+//     {id: '7', nombre: "RTX 3080 Ti ", precio: 2000, categoria: "GPU"},
+//     {id: '8', nombre: "RTX 3070 GAMING", precio: 1700, categoria: "GPU"},
+//     {id: '9', nombre: "AMD Ryzen 9 5900X", precio: 1550, categoria: "CPU"},
+//     {id: '10', nombre: "AMD Ryzen 7 5800X", precio: 1450, categoria: "CPU"}
+// ]
 
 export const obtenerProduto = (id) => {
     return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            const producto = productos.find((prod) => prod.id === id)
-            if(producto){
-                resolve(producto)
+        const db = getFirestore()
+
+        const itemDoc = doc(db, "items", id)
+
+        getDoc(itemDoc)
+        .then((doc) => {
+            if (doc.exists()) {
+                resolve({id: doc.id, ...doc.data() })
             }else{
-                reject("Todavia no tenemos ese producto")
+                resolve(null)
             }
-        }, 1000)
+        })
+        .catch((error) =>  reject(error))
     })
 }
 
 export const obtenerProdutos = (categor) => {
     return new Promise((resolve, reject) => {
-        setTimeout(() =>{
-            const productoFiltrado = categor ? productos.filter((el) => el.categoria === categor) : productos
-            resolve(productoFiltrado)
-        },1000)
+        const db = getFirestore()
+
+        const itemCollection = collection(db, "items")
+
+        let q;
+        if (categor) {
+            q = query(itemCollection, where("categoria", "==", categor))
+        }else{
+            q = query(itemCollection)
+        }
+
+        getDocs(q)
+        .then((snapshot) => {
+            const productos = snapshot.docs.map((doc) =>{
+                return {id : doc.id, ...doc.data()}
+            })  
+            resolve(productos)
+        })
+        .catch((error) => reject(error))
     })
 }
